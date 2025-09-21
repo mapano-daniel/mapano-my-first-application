1e1cd735b47a8cb9f7b909218eb744d1b2e4e6ff
mapano-daniel <mapano.daniel@gmail.com>
2025-09-22 01:16:48 +0800

    Chapter 4: Implement Eloquent relationships

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Tag;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -11,13 +12,14 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    public function run(): void 
+    { 
+        // \App\Models\User::factory(10)->create(); 
+        Tag::truncate();
+        $tags = \App\Models\Tag::factory(10)->create(); 
+    
+        \App\Models\Job::factory(20)->create()->each(function($job) use ($tags) { 
+            $job->tags()->attach($tags->random(2)); 
+        }); 
+    } 
 }
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -6,9 +6,15 @@
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> 
         @foreach ($jobs as $job)
             <div class="glass-card p-6 flex flex-col items-center text-center">
-                <h2 class="text-xl font-semibold text-gray-700 mb-2">{{ $job['title'] }}</h2>
+                <h2 class="text-xl font-semibold text-gray-700 mb-1">{{ $job['title'] }}</h2>
+                <p class="text-gray-500 mt-0">{{ $job->employer->name }}</p>
                 <p class="text-3xl font-bold text-gray-900">{{ $job['salary'] }}</p>
                 <p class="text-gray-500 mt-1">per year</p>
+                <div class="px-4 py-4">
+                    @foreach($job->tags as $tag)
+                        <span class="bg-gray-200 text-gray-700 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded-full">{{ $tag->name }}</span>
+                    @endforeach
+                </div>
             </div>
         @endforeach
     </div>
